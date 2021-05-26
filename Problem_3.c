6d89cbc86eb36e2b279dfc4865f4088f851adfec
Manjunath212web <>
2021-05-26 12:12:49 +0000

    pyramid of staers

--- Problem_3.c
+++ Problem_3.c
@@ -7,7 +7,11 @@ int main() {
     scanf("%d", &number);
 
     // Write your code here
-    
+    if(number%2==0)
+    printf("%d is even\n",number);
+
+    else
+    printf("%d is odd\n",number);
     
    
     return 0;
--- Problem_4.c
+++ Problem_4.c
@@ -5,12 +5,19 @@
 
 int main()
 {
-  	int n;
-
-  	scanf("%d", &n);
+  	int n, i, j;
+    printf("enter the value of n\n");
+    scanf("%d", &n);
 
   	//Write your code here
-  	
+  	for(i=1;i<=n;i++)
+    {
+      for(j=1;j<=i;j++)
+      {
+        printf("$");
+      }
+      printf("\n");
+    }
 
   	return 0;
 }
